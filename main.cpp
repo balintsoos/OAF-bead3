a6c5cbe6287c3839468c7e4620587a550a733ead
Balint Soos <soosbalint95@gmail.com>
2015-11-11 00:31:47 +0100

    input handler

--- main.cpp
+++ main.cpp
@@ -8,12 +8,57 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include "animal.h"
 
 using namespace std;
 
+struct AnimalStruct
+{
+	string name;
+	char type;
+	int happiness;
+};
+
 int main ()
 {
+	// Input Handler
+	ifstream input("input.txt");
+	
+	int numberOfAnimals;
+	input >> numberOfAnimals;
+
+	AnimalStruct * animalArray = new AnimalStruct[numberOfAnimals];
+
+	for (int i = 0; i < numberOfAnimals; ++i)
+	{
+		input >> animalArray[i].type;
+		input >> animalArray[i].name;
+		input >> animalArray[i].happiness;
+	}
+
+	string days;
+	input >> days;
+
+	input.close();
+
+	// Create Animal objects
+	Animal * collection = new Animal[numberOfAnimals];
+	for (int i = 0; i < numberOfAnimals; ++i)
+	{
+		if (animalArray[i].type == 'H')
+		{
+
+		}
+		else if (animalArray[i].type == 'M')
+		{
+
+		}
+		else if (animalArray[i].type == 'K')
+		{
+
+		}
+	}
 	
 	return 0;
 }
